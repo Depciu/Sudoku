71ba8f184e829f4c87706f5d0b7618436cefa11e
Dawid Kaczmarek <d.kaczmarek29@gmail.com>
2018-02-09 15:24:13 +0100

    Add components files

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Board from './Board.js';
 
 class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <p>Hello</p>
+      <div className='app'>
+        <h1>Sudoku</h1>
+        <Board />
+        <div className="buttons">
+          <button>Check</button>
+          <button>New Game</button>
+          <button>Solve</button>
+          <button>Restart</button>
+        </div>
       </div>
     );
   }
---
+++ src/components/Board.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Title from './Title.js';
+
+class Board extends React.Component {
+  render(){
+    return (
+      <Title />
+    );
+  }
+}
+
+export default Board
